e476bb646d8ef07ae88a6b17038f2a022f507385
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-02-09 01:46:08 +0100

    cards

---
+++ cards.js
@@ -0,0 +1,17 @@
+const dataList = [
+  {
+    "title": "Made of Air",
+    "description": "Activating the Made of Air Brand",
+    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/12/03164336/01_MOA__Hero542.jpg"
+  },
+  {
+    "title": "Ambu",
+    "description": "Bringing Innovative Med-Tech to Market with Ambu",
+    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/09/15155137/21_Case-Study-Ambu-01.png"
+  },
+  {
+    "title": "Innocor",
+    "description": "Reset company strategy for a disrupted market",
+    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/10/20133727/21_Case-Study-Innocor.jpg"
+  }
+]
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const nav = document.getElementById('nav')
   var desc = document.getElementById('desc')
 
-  var position = desc.offsetTop /* - desc.offsetHeight */
+  var position = desc.offsetTop - desc.offsetHeight
 
   window.addEventListener("scroll", function () {
     var st = window.pageYOffset || document.documentElement.scrollTop;
@@ -16,12 +16,33 @@ document.addEventListener("DOMContentLoaded", function () {
     position = st;
   })
 
-  fetch("/cards.json")
-    .then(res => res.json())
-    .then((data) => {
-      for (let cards in data) {
-        console.log(data[cards])
-      }
-    })
+  let caseCards = document.getElementById('caseCards')
+  for (let i in dataList) {
+    let card = document.createElement('div')
+    let a = document.createElement('a')
+    let div = document.createElement('div')
+    let image = document.createElement('img')
+    let content = document.createElement('div')
+    let h5 = document.createElement('h5')
+    let p = document.createElement('p')
 
+    card.classList.add('card')
+    div.classList.add('cardImage_container')
+
+    image.src = dataList[i].image
+    image.classList.add('cardImage')
+
+    content.classList.add('design_card_content')
+
+    h5.innerText = dataList[i].title
+    p.innerText = dataList[i].description
+
+    caseCards.appendChild(card)
+    card.appendChild(a)
+    a.appendChild(div)
+    a.appendChild(content)
+    div.appendChild(image)
+    content.appendChild(h5)
+    content.appendChild(p)
+  }
 })
