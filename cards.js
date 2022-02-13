94c701a6c45589321499fa49b955978c2bb0af1b
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-02-14 00:50:45 +0100

    images

--- cards.js
+++ cards.js
@@ -2,62 +2,62 @@ const CardsList = [
   {
     "title": "Made of Air",
     "description": "Activating the Made of Air Brand",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/12/03164336/01_MOA__Hero542.jpg"
+    "image": "/img/card.jpg"
   },
   {
     "title": "Ambu",
     "description": "Bringing Innovative Med-Tech to Market with Ambu",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/09/15155137/21_Case-Study-Ambu-01.png"
+    "image": "/img/card1.png"
   },
   {
     "title": "Innocor",
     "description": "Reset company strategy for a disrupted market",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/10/20133727/21_Case-Study-Innocor.jpg"
+    "image": "/img/card2.jpg"
   },
   {
     "title": "Sesame",
     "description": "Delivery, Designed by Restaurants",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/07/12161457/21_Case-Study-Sesame-01_thumbnail.jpg"
+    "image": "/img/card3.jpg"
   },
   {
     "title": "MeMed",
     "description": "Revolutionizing Testing at the Point-of-Need",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/03/22120457/21-Marketing-Case-MeMed-Press-Release-Thumb-02.jpg"
+    "image": "/img/card4.jpg"
   },
   {
     "title": "Vyoh",
     "description": "A Sustainable Solution for Indoor Air Pollution",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/02/04145333/Thumbnail_Vyoh.jpg"
+    "image": "/img/card5.jpg"
   },
   {
     "title": "Campfire",
     "description": "Investing in the Future of Design Collaboration",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/03/09164225/Thumbnail_CAMPFIRE.jpg"
+    "image": "/img/card6.jpg"
   },
   {
     "title": "Companion",
     "description": "Understanding Animals Through Technology",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/03/21071725/21-Marketing-Case-Companion-Desktop-01-1.jpg"
+    "image": "/img/card7.jpg"
   },
   {
     "title": "The Norwegian Sea Rescue Society",
     "description": "Co-creating a Sustainability Strategy",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/08/23181412/21_Case-Study-Norwegian-01.png"
+    "image": "/img/card8.png"
   },
   {
     "title": "HealthHero",
     "description": "Designing the Next Era of Telehealth for Europe",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2021/02/21103043/21_Marketing_Case_HealthHero-Hero-1.jpg"
+    "image": "/img/card9.jpg"
   },
   {
     "title": "Dispatch Goods",
     "description": "Creating a brand to take down single-use takeout containers",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2020/11/21161826/DG-Thumbnail-1.jpg"
+    "image": "/img/card10.jpg"
   },
   {
     "title": "JustAir",
     "description": "Designing a consumer air mask for pandemic times",
-    "image": "https://frogdesign.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2020/11/21152714/Just-Air_HERO-scaled-3.jpg"
+    "image": "/img/card11.jpg"
   },
 ]
 
